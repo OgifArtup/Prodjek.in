9d189f4eeb747cc2444c6c7828d7909a90145af3
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-06-09 14:41:40 +0700

    Changed SQL Database File

--- database/prodjek_in.sql
+++ database/prodjek_in.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2023 at 04:59 PM
+-- Generation Time: Jun 09, 2023 at 09:40 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,174 @@ SET time_zone = "+00:00";
 --
 -- Database: `prodjek_in`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment_lists`
+--
+
+CREATE TABLE `assignment_lists` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `task_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `assignment_lists`
+--
+
+INSERT INTO `assignment_lists` (`id`, `user_id`, `task_id`) VALUES
+(7, 1, 7),
+(8, 1, 8),
+(9, 2, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(3, '2023_05_12_114103_create__workspace_list_table', 1),
+(4, '2023_05_12_114517_create_workspace_table', 1),
+(5, '2023_05_12_114728_create_tasks_table', 1),
+(6, '2023_05_12_115330_create__assignment_list_table', 1),
+(7, '2023_05_12_124248_create__workspace_list_table', 1),
+(8, '2023_05_12_124303_create_tasks_table', 1),
+(9, '2023_05_12_124313_create__assignment_list_table', 1),
+(10, '2023_06_07_125210_create_notifications_table', 1),
+(11, '2023_06_07_125542_create_notifications_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `workspace_id` bigint(20) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `detail` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `user_id`, `workspace_id`, `type`, `detail`, `status`, `created_at`, `updated_at`) VALUES
+(7, 1, 1, 'notification', 'New Important Task Assigned in Test Project Workspace = Test Task, User can use the project. Deadline on 2023-06-14', 'pending', '2023-06-07 07:18:33', '2023-06-07 07:18:33'),
+(8, 1, 1, 'notification', 'New Urgent Task Assigned in Test Project Workspace = Create Homepage, Make homepage for user landing page. Deadline on 2023-06-09', 'pending', '2023-06-07 07:19:18', '2023-06-07 07:19:18'),
+(9, 2, 1, 'notification', 'New Urgent Task Assigned in Test Project Workspace = Create Homepage, Make homepage for user landing page. Deadline on 2023-06-09', 'pending', '2023-06-07 07:19:18', '2023-06-07 07:19:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `workspace_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `due_date` date NOT NULL,
+  `priority` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `workspace_id`, `name`, `description`, `due_date`, `priority`, `status`) VALUES
+(7, 1, 'Test Task', 'User can use the project', '2023-06-14', 'Important', 'Ongoing'),
+(8, 1, 'Create Homepage', 'Make homepage for user landing page', '2023-06-09', 'Urgent', 'Ongoing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `google_id` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `google_id`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Figo Lego', 'Figo Lego', 'figo.lego@gmail.com', '102995596866244433469', NULL, '$2y$10$WH/tFMT/kwfP8NF3vU6qgOO4Mtcdwvz76ZQtBftM9MuVO8/GrAHhO', NULL, '2023-06-07 06:14:26', '2023-06-07 06:14:31'),
+(2, 'Test User', 'TestUser1', 'test@gmail.com', '123456789012345678901', NULL, '$2y$10$7tP//GBhzqEpy/PjpWM1Be0Y1AoTL89T7ICb8TrayY8iXAtmJHzv2', NULL, '2023-06-07 06:15:50', '2023-06-07 06:15:50'),
+(3, 'Test User 2', 'TestUser2', 'test2@gmail.com', '420420420420420420420', NULL, '$2y$10$xU2lM0K.blFZBn5k.hb2VO7mJNhaRCHuvWj7KEuu/Wxv8OqLuvsj2', NULL, '2023-06-07 06:15:50', '2023-06-07 06:15:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workspaces`
+--
+
+CREATE TABLE `workspaces` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `team_name` varchar(255) NOT NULL,
+  `project_detail` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `workspaces`
+--
+
+INSERT INTO `workspaces` (`id`, `name`, `team_name`, `project_detail`) VALUES
+(1, 'Test Project', 'Team Test', 'This project is for testing purposes'),
+(2, 'Alexandria Skyline', 'M.E.T.A', 'To make the biggest collection of knowledge');
 
 -- --------------------------------------------------------
 
@@ -49,6 +217,57 @@ INSERT INTO `workspace_lists` (`id`, `user_id`, `workspace_id`, `role`) VALUES
 --
 
 --
+-- Indexes for table `assignment_lists`
+--
+ALTER TABLE `assignment_lists`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `workspace_id` (`workspace_id`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `workspace_id` (`workspace_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_google_id_unique` (`google_id`);
+
+--
+-- Indexes for table `workspaces`
+--
+ALTER TABLE `workspaces`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `workspace_lists`
 --
 ALTER TABLE `workspace_lists`
@@ -61,6 +280,48 @@ ALTER TABLE `workspace_lists`
 --
 
 --
+-- AUTO_INCREMENT for table `assignment_lists`
+--
+ALTER TABLE `assignment_lists`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `workspaces`
+--
+ALTER TABLE `workspaces`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `workspace_lists`
 --
 ALTER TABLE `workspace_lists`
@@ -69,6 +330,26 @@ ALTER TABLE `workspace_lists`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `assignment_lists`
+--
+ALTER TABLE `assignment_lists`
+  ADD CONSTRAINT `assignment_lists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `assignment_lists_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`workspace_id`) REFERENCES `workspaces` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`workspace_id`) REFERENCES `workspaces` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `workspace_lists`
